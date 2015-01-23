27c7a819eb07b07a4e4a7ce2a7d84b35d0c5cbb5
Rawan Alharbi <Rawan@dhcp-10-101-80-33.wireless.northwestern.private>
2015-01-22 20:14:30 -0600

    production version hosted on heroku

--- swapit/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/rawanalharbi/Desktop/swapit/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- swapit/src/mvp_landing/live/wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mvp_landing.settings'
-
-from django.core.wsgi import get_wsgi_application
-
-application = get_wsgi_application()
